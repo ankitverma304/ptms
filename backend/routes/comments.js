09fe5e89109a893396a2983ef8e266fcb8d83b21
ankitverma304 <104671114+ankitverma304@users.noreply.github.com>
2026-05-18 16:22:03 +0530

    Delete backend directory

--- backend/routes/comments.js
+++
@@ -1,9 +0,0 @@
-// routes/comments.js
-const express = require('express');
-const r = express.Router();
-const ctrl = require('../controllers/commentController');
-const { authenticate } = require('../middleware/auth');
-r.use(authenticate);
-r.patch('/:id',  ctrl.update);
-r.delete('/:id', ctrl.remove);
-module.exports = r;
